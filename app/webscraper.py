a4a758d2ddbdbfa71cb296325a6b50d67adaf45c
Alex <alexbruch@gmail.com>
2023-11-28 19:29:43 +0100

    ja

--- app/webscraper.py
+++ app/webscraper.py
@@ -19,8 +19,12 @@ driver = webdriver.Chrome(options=chrome_options)
 
 ## DO STUFF
 
+driver.get('https://www.ls-tc.de/de/')
+
 xpath_string = '//*[@id="chart3push"]/span[2]/span'
+element = driver.find_element(By.XPATH,xpath_string)
 element = driver.find_element_by_xpath(xpath_string)
+
 logging.debug('element ' + str(element))
 
 # close chromedriver and display
