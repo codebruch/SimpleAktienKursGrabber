f78a15b73de4ceda8e793723cbd7c122389a0ffe
alex <alexbruch@gmail.com>
2023-11-28 18:33:32 +0000

    Update 'build.sh'

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker build --pull --rm -f "Dockerfile" -t grabber:latest "."
 PIMAGE=$(docker images --filter="reference=grabber:latest" --quiet) 
 docker tag $PIMAGE registry.registry.lan/grabber:release 
 docker push registry.registry.lan/grabber:release
-kubectl apply -f Deployment.yaml
+kubectl rollout restart -n default deployment grabber
