8c186a3d311f7778ad288f724e157a4bf1dd5b8b
Alex <alexbruch@gmail.com>
2023-11-29 20:50:07 +0100

    SP500

--- app/webscraper.py
+++ app/webscraper.py
@@ -19,52 +19,109 @@ import queue
 import random
 import threading
 import time
+import sys
 from websockets.sync.client import connect
+from selenium.common import exceptions  
 
-def grabDAXproducer():
-    r = redis.Redis(host='redis.lan', port=6379, decode_responses=True)
+class webscraper():
 
-    # set xvfb display since there is no GUI in docker container.
-    display = Display(visible=0, size=(800, 600))
-    display.start()
+    def grabDAXproducer():
+        r = redis.Redis(host='redis.lan', port=6379, decode_responses=True)
 
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+        # set xvfb display since there is no GUI in docker container.
+        display = Display(visible=0, size=(800, 600))
+        display.start()
 
-    logging.debug('building session')
-    driver = webdriver.Chrome(options=chrome_options)
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
 
-    ## DO STUFF
-    url = 'https://www.ls-tc.de/de/'
-    driver.get(url)
-    logging.debug('opened: ' + url)
+        logging.debug('building session')
+        driver = webdriver.Chrome(options=chrome_options)
 
-    #with connect("ws://localhost:8765") as websocket:
-    #    logging.debug('opened websocket connection')
+        ## DO STUFF
+        url = 'https://www.ls-tc.de/de/'
+        driver.get(url)
+        logging.debug('opened: ' + url)
 
-    while True:
-        xpath_string = '//*[@id="chart3push"]/span[2]/span'
-        element = driver.find_element(By.XPATH,xpath_string).text
+        #with connect("ws://localhost:8765") as websocket:
+        #    logging.debug('opened websocket connection')
 
-        logging.debug('DAX element ' + str(element))
-        dax = "DAX: "+str(element)
-        ##websocket.send(dax)
-        r.publish("DAX", str(element))
-        
-        time.sleep(3)
+        while True:
+            xpath_string = '//*[@id="chart3push"]/span[2]/span'
+            element = driver.find_element(By.XPATH,xpath_string).text
 
-        # close chromedriver and display
-    driver.quit()
-    display.stop()
+            logging.debug('DAX element ' + str(element))
+            dax = "DAX: "+str(element)
+            ##websocket.send(dax)
+            r.publish("DAX", str(element))
+            
+            time.sleep(1)
+
+            # close chromedriver and display
+        driver.quit()
+        display.stop()
+
+    def grabSP500producer():
+        r = redis.Redis(host='redis.lan', port=6379, decode_responses=True)
+
+        # set xvfb display since there is no GUI in docker container.
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
+        logging.debug('building session')
+        driver = webdriver.Chrome(options=chrome_options)
+
+        ## DO STUFF
+        url = 'https://www.boerse.de/realtime-kurse/SundP-500-Aktien/US78378X1072'
+        driver.get(url)
+        logging.debug('opened: ' + url)
+
+        #with connect("ws://localhost:8765") as websocket:
+        #    logging.debug('opened websocket connection')
+
+        while True:
+            xpath_string = '//*[@id="content_container"]/div/div/div[1]/div/div[1]/div/div[1]/div[1]/div/div[2]/div[1]/span/span[1]'
+
+            try:  
+                element = driver.find_element(By.XPATH,xpath_string).text
+                
+                
+            except exceptions.StaleElementReferenceException as e:
+                logging.debug('StaleElementReferenceException: ' + str(e))
+                pass  
+            
+
+            logging.debug('SP500 element ' + str(element))
+            SP500 = "SP500: "+str(element)
+            ##websocket.send(dax)
+            r.publish("SP500", str(element))
+            
+            time.sleep(1)
+
+            # close chromedriver and display
+        driver.quit()
+        display.stop()
 
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(filename='grabber.log',format=format, level=logging.INFO,
+    logging.basicConfig(stream=sys.stdout,format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
     logging.getLogger().setLevel(logging.DEBUG)
-    grabDAXproducer()
-    
+ 
+
+    thread = threading.Thread(target = webscraper.grabDAXproducer)
+    thread2 = threading.Thread(target = webscraper.grabSP500producer)
+    thread.start()
+    logging.debug("thread1 start: " + str(thread))
+    thread2.start()
+    logging.debug("thread2 start: " + str(thread2))
+    thread.join()
+    logging.debug("thread finished...exiting")
 
